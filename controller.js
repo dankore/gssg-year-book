a84bf279b16e8ca579936e2f4b999984a59616d3
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-01-15 10:18:38 -0600

    form test

--- controller.js
+++ controller.js
@@ -6,7 +6,7 @@ exports.home = (req, res) => {
 }
 
 exports.createProfile = (req, res) => {
-  console.log(req.body.firstName)
+  console.log(req.body.firstName, req.body.lastName, req.body.email, req.body.password)
   res.send("Thank for for submitting your profile")
 }
 
