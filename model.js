9e57d241ca66fed24f92a9f8203e9194d0e3f16a
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-01-17 07:41:26 -0600

    session set up begin

--- model.js
+++ model.js
@@ -5,69 +5,69 @@ const bcryptjs = require('bcryptjs');
 let User = class user {
   constructor(data) {
     this.data = data,
-    this.errors = []
+      this.errors = []
   }
 };
 
-User.prototype.validateUserRegistration = function(){
-  return new Promise(async (resolve, reject) =>{
-     // check for empty boxes
-    if(this.data.firstName.length == ""){
+User.prototype.validateUserRegistration = function () {
+  return new Promise(async (resolve, reject) => {
+    // check for empty boxes
+    if (this.data.firstName.length == "") {
       this.errors.push("First name is required.")
     }
-    if(this.data.lastName.length == ""){
+    if (this.data.lastName.length == "") {
       this.errors.push("Last name is required.")
     }
-    if(this.data.email.length == ""){
+    if (this.data.email.length == "") {
       this.errors.push("Email is required.")
     }
-    if(this.data.password.length == ""){
+    if (this.data.password.length == "") {
       this.errors.push("Password is required.")
     }
 
     // check for non-allowed inputs
-    if(this.data.firstName.length != "" && !validator.isAlphanumeric(this.data.firstName)){
-      errors.push("First name can only contain letters and numbers.")
+    if (this.data.firstName.length != "" && !validator.isAlphanumeric(this.data.firstName)) {
+      this.errors.push("First name can only contain letters and numbers.")
     }
-    if(this.data.firstName.length != "" && !validator.isAlphanumeric(this.data.lastName)){
-      errors.push("Last name can only contain letters and numbers.")
+    if (this.data.firstName.length != "" && !validator.isAlphanumeric(this.data.lastName)) {
+      this.errors.push("Last name can only contain letters and numbers.")
     }
-    if(this.data.firstName.length != "" && !validator.isEmail(this.data.email)){
-      errors.push("Email can only contain letters and numbers.")
+    if (this.data.firstName.length != "" && !validator.isEmail(this.data.email)) {
+      this.errors.push("Email can only contain letters and numbers.")
     }
 
     // check to see if email is valid and not taken
-    if(validator.isEmail(this.data.email)){
-      let emailExist = await usersCollection.findOne({email: this.data.email});
+    if (validator.isEmail(this.data.email)) {
+      let emailExist = await usersCollection.findOne({ email: this.data.email });
 
-      if(emailExist){
+      if (emailExist) {
         this.errors.push("That email is already been used.")
       }
     }
     resolve();
   });
- 
+
 }
 
-User.prototype.cleanUp = function(){
-  if(typeof this.data.firstName != 'string'){
+User.prototype.cleanUp = function () {
+  if (typeof this.data.firstName != 'string') {
     this.data.firstName = "";
   }
-  if(typeof this.data.lastName != 'string'){
+  if (typeof this.data.lastName != 'string') {
     this.data.lastName = "";
   }
-  if(typeof this.data.email != 'string'){
+  if (typeof this.data.email != 'string') {
     this.data.email = "";
   }
 }
 
 User.prototype.register = function () {
 
-  return new Promise(async(resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     this.cleanUp();
     await this.validateUserRegistration();
 
-    if(!this.errors.length){
+    if (!this.errors.length) {
       let salt = bcryptjs.genSaltSync(10);
       this.data.password = bcryptjs.hashSync(this.data.password, salt);
       await usersCollection.insertOne(this.data);
@@ -75,19 +75,19 @@ User.prototype.register = function () {
     } else {
       reject(this.errors);
     }
-    
+
   })
- 
+
 }
 
-User.prototype.login = function(){
+User.prototype.login = function () {
 
-  return new Promise(async (resolve, reject)=>{
+  return new Promise(async (resolve, reject) => {
     this.cleanUp();
-    await usersCollection.findOne({email: this.data.email}, (err, attemptedUser) => {
+    await usersCollection.findOne({ email: this.data.email }, (err, attemptedUser) => {
       // attemptedUser: if email exist, attemptedUser is the whole document
       // if user does NOT exist dont bother making a query
-      if(attemptedUser && bcryptjs.compareSync(this.data.password, attemptedUser.password)){
+      if (attemptedUser && bcryptjs.compareSync(this.data.password, attemptedUser.password)) {
         resolve();
       } else {
         reject();
@@ -95,6 +95,6 @@ User.prototype.login = function(){
     })
 
   })
-  
+
 }
 module.exports = User;
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const session = require('express-session');
 const server = express();
+
+
+let sessionOptions = session({
+    secret: "JavaScript is soooo cool",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }
+});
+
+server.use(sessionOptions);
 const router = require('./router');
 
 
