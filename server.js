7b047338249e0737b511bd228b1290f9b0b9506e
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-01-19 02:59:53 -0600

    btn styles and makde express display images

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const router = require('./router');
 
 server.set("views", "view");
 server.set("view engine", "ejs");
+server.use(express.static('./public'))
 
 server.use(express.urlencoded({ extended: false }))
 server.use(express.json());
