3bc9cde9fda2c42b266cb9a284785d9d035aac6a
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-01-19 03:36:09 -0600

    Wait for validation to complete before registeration

--- controller.js
+++ controller.js
@@ -2,28 +2,29 @@ const User = require('./model');
 
 exports.home = (req, res) => {
   const loginErrors = req.flash('errors');
-  res.render('homePage', { errors: loginErrors, user: req.session.user})
+  res.render('homePage', { errors: loginErrors, user: req.session.user })
 }
 
 exports.registrationPage = (req, res) => {
- const registrationErrors = req.flash("reqError")
+  const registrationErrors = req.flash("reqError")
   res.render('registrationPage', { reqErrors: registrationErrors, user: req.session.user });
 }
 
 exports.registrationSubmission = (req, res) => {
   let user = new User(req.body);
-  user.register();
-
-  if (user.errors.length) {
-    user.errors.forEach(function (error) {
+  user.register().then(() => {
+    req.session.user = { email: user.data.email }
+    req.session.save(async function () {
+      await res.redirect('/')
+    })
+  }).catch((regErrors) => {
+    regErrors.forEach(function (error) {
       req.flash('reqError', error)
     })
-    req.session.save(async function(){
-     await res.redirect('/register')
+    req.session.save(async function () {
+      await res.redirect('/register')
     })
-  } else {
-    res.send("Congrats there no errors.");
-  }
+  })
 
 }
 
--- model.js
+++ model.js
@@ -10,7 +10,8 @@ let User = class user {
 };
 
 User.prototype.validateUserRegistration = function () {
-      // check for empty boxes
+  return new Promise(async (resolve, reject) => {
+    // check for empty boxes
     if (this.data.firstName.length == "") {
       this.errors.push("First name is required.")
     }
@@ -27,16 +28,16 @@ User.prototype.validateUserRegistration = function () {
       this.errors.push("Year of graduation is required.")
     }
     // check for length
-    if(this.data.password.length < 4){
+    if (this.data.password.length < 4) {
       this.errors.push("Password should be at least 4 characters.")
     }
-    if(this.data.password.length > 50){
+    if (this.data.password.length > 50) {
       this.errors.push("Password cannot exceed 50 characters.")
     }
-    if(String(this.data.year).length < 4){
+    if (String(this.data.year).length < 4) {
       this.errors.push("Year should not be less than 4 characters.")
     }
-    if(String(this.data.year).length > 4){
+    if (String(this.data.year).length > 4) {
       this.errors.push("Year should not be greater than 4 characters.")
     }
 
@@ -53,20 +54,25 @@ User.prototype.validateUserRegistration = function () {
     if (this.data.year.length != "" && !validator.isNumeric(this.data.year)) {
       this.errors.push("Year can only be numbers.")
     }
-    
-    // check to see if email is valid and not taken
 
+    // if email is valid, check to see if it is taken
+    if (validator.isEmail(this.data.email)) {
+      let emailExist = await usersCollection.findOne({ email: this.data.email });
+      if (emailExist) { this.errors.push('That email is already taken.') }
+    }
+    resolve()
+  })
 }
 
-User.prototype.login = function(){
-  return new Promise((resolve, reject)=> {
+User.prototype.login = function () {
+  return new Promise((resolve, reject) => {
     this.cleanUp();
-    usersCollection.findOne({email: this.data.email}).then((attemptedUser)=>{
-      if(attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)){
-      resolve("Congrats!")
-    } else {
-      reject("Invalid email/password!")
-    }
+    usersCollection.findOne({ email: this.data.email }).then((attemptedUser) => {
+      if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+        resolve("Congrats!")
+      } else {
+        reject("Invalid email/password!")
+      }
     }).catch(() => {
       reject("Please try again later.")
     })
@@ -86,20 +92,25 @@ User.prototype.cleanUp = function () {
 }
 
 User.prototype.register = function () {
+  return new Promise(async (resolve, reject) => {
     //Make sure email is string
     this.cleanUp()
     // Step #1: Validate user data
-    this.validateUserRegistration();
+    await this.validateUserRegistration();
 
     // Step #2: Only if there no validation error
     // then save the user data into the database
     if (!this.errors.length) {
-        // Hash user password
-        let salt = bcrypt.genSaltSync(10);
-        this.data.password = bcrypt.hashSync(this.data.password, salt)
-        usersCollection.insertOne(this.data)
+      // Hash user password
+      let salt = bcrypt.genSaltSync(10);
+      this.data.password = bcrypt.hashSync(this.data.password, salt);
+      await usersCollection.insertOne(this.data);
+      resolve();
+    } else {
+      reject(this.errors);
     }
 
+  })
 }
 
 module.exports = User;
