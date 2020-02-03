0eddf81694d16c33266ce479b3fcf1f7123ebdbc
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-02-03 16:13:24 -0600

    Toggle switches

--- frontend-js/modules/toggleStats.js
+++ frontend-js/modules/toggleStats.js
@@ -2,19 +2,38 @@ export default class ToggleStats {
   constructor() {
     this.btnPluSign = document.querySelector("#plus-sign");
     this.statsContainer = document.querySelector("#stats-container");
+    this.body = document.querySelector("body");
+    this.toolTip = document.querySelector("#tool-tip");
     this.events();
   }
   // EVENTS
   events() {
-    this.btnPluSign.addEventListener("click", () => this.toggleStats());
+    this.btnPluSign.addEventListener("click", e => this.toggleStats(e));
+    this.body.addEventListener("click", () => this.closeStatsModel());
+    this.btnPluSign.addEventListener("mouseover", e => this.showToolTip(e));
+    this.body.addEventListener("mouseover", () => this.closeToolTip());
   }
 
   // METHODS
-  toggleStats() {
+  toggleStats(e) {
+    e.stopPropagation();
     if (this.statsContainer.style.display == "none") {
       this.statsContainer.style.display = "block";
     } else {
       this.statsContainer.style.display = "none";
     }
   }
+
+  closeStatsModel() {
+    this.statsContainer.style.display = "none";
+  }
+
+  showToolTip(e) {
+    e.stopPropagation();
+    this.toolTip.style.display = "block";
+  }
+  closeToolTip() {
+    this.toolTip.style.display = "none";
+  }
+  // END CLASS
 }
