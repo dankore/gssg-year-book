9f24f4325ace3ae91d875510b5217055d921d4a2
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-02-09 04:13:48 +0000

    stats

--- frontend-js/modules/toggleStats.js
+++ frontend-js/modules/toggleStats.js
@@ -2,6 +2,7 @@ export default class ToggleStats {
   constructor() {
     this.arrowDown = document.querySelector("#arrow-down");
     this.statsContainer = document.querySelector("#myChart");
+    this.arrowSigns = document.querySelector("#arrows");
     this.events();
   }
   // EVENTS
@@ -14,8 +15,10 @@ export default class ToggleStats {
     console.log("hi")
     if (this.statsContainer.style.display == "none") {
       this.statsContainer.style.display = "block";
+      this.arrowSigns.innerHTML = "&#8911";
     } else {
       this.statsContainer.style.display = "none";
+       this.arrowSigns.innerHTML = "&#8910";
     }
   }
   // END CLASS
