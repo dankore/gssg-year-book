e8dfb424cf4388205ad007b03f5dd67b1bed9931
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-02-15 09:19:19 -0600

    registration form live validation feature set up: first name val #1

--- frontend-js/modules/registrationFormLiveValidation.js
+++ frontend-js/modules/registrationFormLiveValidation.js
@@ -1,20 +1,70 @@
 export default class RegistrationFormLiveValidation {
-    constructor(){
-        this.allFields = document.querySelectorAll("#registration-form .form-control");
-        this.insertValidationElements()
-        this.events();
-    }
-    //EVENTS
-    events(){
-        
-    }
+  constructor() {
+    this.allFields = document.querySelectorAll(
+      "#registration-form .form-control"
+    );
+    this.insertValidationElements();
+    this.firstName = document.querySelector("#first-name");
+    this.firstName.previousValue = "";
+    this.events();
+  }
+  //EVENTS
+  events() {
+    this.firstName.addEventListener("keyup", () => {
+      this.isDifferent(this.firstName, this.firstNameHandler);
+    });
+  }
 
-    // METHODS
-    insertValidationElements(){
-        this.allFields.forEach(item =>{
-            item.insertAdjacentHTML('beforebegin', '<div class="text-red-600 border border-red-600 bg-red-200">Adamu</div>')
-        })
+  // METHODS
+  isDifferent(el, handler) {
+    if (el.previousValue != el.value) {
+      handler.call(this);
     }
+    el.previousValue = el.value;
+  }
 
-    // END CLASS
+  firstNameHandler() {
+    this.firstName.errors = false;
+    this.firstNameImmediately();
+    clearTimeout(this.firstName.timer);
+    this.firstName.timer = setTimeout(() => this.firstNameAfterDelay(), 3000);
+  }
+
+  firstNameImmediately() {
+    if (
+      this.firstName.value != "" &&
+      !/^([a-zA-Z0-9]+)$/.test(this.firstName.value)
+    ) {
+      this.showValidationError(
+        this.firstName,
+        "First Name can only contain letters and numbers."
+      );
+    }
+    if (!this.firstName.errors) {
+      this.hideValidationError(this.firstName);
+    }
+  }
+  hideValidationError(el) {
+    el.previousElementSibling.classList.remove("liveValidationMessage--show");
+  }
+  showValidationError(el, message) {
+    el.previousElementSibling.innerText = message;
+    el.previousElementSibling.classList.add("liveValidationMessage--show");
+    el.errors = true;
+  }
+
+  firstNameAfterDelay() {
+    // alert("after delay");
+  }
+
+  insertValidationElements() {
+    this.allFields.forEach(item => {
+      item.insertAdjacentHTML(
+        "beforebegin",
+        '<div class="bg-red-100 border border-red-400 text-red-700 text-center text-xs rounded liveValidationMessage--hide">ada</div>'
+      );
+    });
+  }
+
+  // END CLASS
 }
