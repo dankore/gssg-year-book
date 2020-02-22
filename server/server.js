63a62e9c2527e0035c70f8ab08d4ebde909dc63a
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-02-22 04:52:42 -0600

    refac to last master HEAD

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// require('dotenv').config();
 const express = require("express"),
   session = require("express-session"),
   MongoStore = require("connect-mongo")(session),
@@ -9,13 +8,94 @@ const express = require("express"),
   compression = require("compression"),
   User = require("./models/model"),
   passport = require("passport"),
-  GoogleStrategyMiddleware = require("./middlewares/google.passport"),
-  FacebookStrategyMiddleware = require("./middlewares/facebook.passport");
+  FacebookStrategy = require("passport-facebook").Strategy,
+  GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
 // PASSPORT
-server.use(GoogleStrategyMiddleware); // GOOGLE
 
-server.use(FacebookStrategyMiddleware); // FACEBOOK
+// GOOGLE
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: `${process.env.GOOGLE_CLIENT_ID}`,
+      clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+      callbackURL: "https://www.gssgcontactbook.com/google-login/callback"
+    },
+    function(accessToken, refreshToken, user, cb) {
+      User.doesEmailExists(user._json.email)
+        .then(userBool => {
+          console.log("Server 29. New user: " + userBool);
+          if (userBool) {
+            // USER EXISTS. LOG IN
+            // CLEAN UP
+            user = {
+              email: user._json.email,
+              returningUser: true
+            };
+            return cb(null, user);
+          } else {
+            // NEW USER. REGISTER
+            // CLEAN UP
+            user = {
+              google_id: user._json.sub,
+              firstName: user._json.given_name,
+              lastName: user._json.family_name,
+              email: user._json.email,
+              year: "1988?",
+              photo: user._json.picture // END
+            };
+            return cb(null, user);
+          }
+        })
+        .catch(err => {
+          console.log("Server 58 " + err);
+        });
+    }
+  )
+);
+
+// FACEBOOK
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: `${process.env.FB_CLIENT_ID}`,
+      clientSecret: `${process.env.FB_CLIENT_SECRET}`,
+      callbackURL: "https://www.gssgcontactbook.com/fb-login/callback",
+      profileFields: ["id", "first_name", "last_name", "email"],
+      enableProof: true
+    },
+
+    function(accessToken, refreshToken, user, cb) {
+      // CHECK IF fbUser exist in database
+      User.doesEmailExists(user._json.email)
+        .then(userBool => {
+          if (userBool) {
+            // USER EXISTS IN DB. LOG IN
+            // CLEAN UP DATA
+            user = {
+              email: user._json.email,
+              returningUser: true
+            };
+            return cb(null, user);
+          } else {
+            // NEW USER. REGISTER
+            // CLEAN UP DATA
+            user = {
+              firstName: user._json.first_name,
+              lastName: user._json.last_name,
+              email: user._json.email,
+              year: "1984?",
+              photo: ""// INITIALIZE PHOTO WITH EMPTY. OTHERWISE BUG HAPPENS
+            };
+            return cb(null, user);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  )
+);
 
 passport.serializeUser(function(user, cb) {
   cb(null, user);
@@ -35,7 +115,7 @@ let sessionOptions = session({
   store: new MongoStore({ client: require("../db") }),
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 1000 * 60 * 60 * 24 * 14, httpOnly: true } // COOKIES EXPIRE2 IN 14 DAYS
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 14, httpOnly: true } // COOKIES EXPIRE IN 14 DAYS
 });
 
 server.set("views", "view");
