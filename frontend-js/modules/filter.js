14983463766bf9727b1dc3897d5bc2dc37780f98
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-02-24 09:46:46 -0600

    Filter set up working. Next refinement and enhancement

--- frontend-js/modules/filter.js
+++ frontend-js/modules/filter.js
@@ -1,20 +1,20 @@
 export default class Filter {
-    constructor(){
-        this.filterIcon = document.getElementById("filter-icon");
-        this.formContainer = document.getElementById("form-container");
-        this.events();
-    }
-    // EVENTS
-    events(){
-        this.filterIcon.addEventListener("click", ()=> this.handleFilterIcon());
-    }
+  constructor() {
+    this.filterIcon = document.getElementById("filter-icon");
+    this.formContainer = document.getElementById("form-container");
+    this.events();
+  }
+  // EVENTS
+  events() {
+    this.filterIcon.addEventListener("click", () => this.handleFilterIcon());
+  }
 
-    // METHODS
-    handleFilterIcon(){
-        if(this.formContainer.style.display == "none"){
-            this.formContainer.style.display = "block";
-        } else {
-            this.formContainer.style.display = "none";
-        }
+  // METHODS
+  handleFilterIcon() {
+    if (this.formContainer.style.display == "none") {
+      this.formContainer.style.display = "block";
+    } else {
+      this.formContainer.style.display = "none";
     }
+  }
 }
