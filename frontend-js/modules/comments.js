424d310b297384fafda02dbeabd14674fd8cc7a6
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-02-28 09:33:42 -0600

    comment edit feature: cancel btn working

--- frontend-js/modules/comments.js
+++ frontend-js/modules/comments.js
@@ -1,26 +1,31 @@
 export default class Comments {
   constructor() {
     this.editBtnComment = document.querySelectorAll("#edit-button");
-    this.containerEditCommentForm = document.querySelectorAll(
-      "#comment-edit-container"
-    );
+    this.cancelEditBtn = document.querySelectorAll("#cancel-edit-comment");
     this.events();
   }
 
   // EVENTS
   events() {
-    Array.prototype.forEach.call(this.editBtnComment, button => {
-      button.addEventListener("click", e => this.handlerContainerClick(e));
+    Array.prototype.forEach.call(this.editBtnComment, editBtn => {
+      editBtn.addEventListener("click", e => this.handlerEditBtnClick(e));
+    });
+    Array.prototype.forEach.call(this.cancelEditBtn, cancelBtn => {
+      cancelBtn.addEventListener("click", e => this.handleCancelBtnClick(e));
     });
   }
 
   // METHODS
-  handlerContainerClick(e) {
-    let currentElem =
+  handlerEditBtnClick(e) {
+    const currentElem =
       e.currentTarget.parentElement.parentElement.parentElement.parentElement
         .children[2];
-
     currentElem.style.display = "block";
+  }
+
+  handleCancelBtnClick(e) {
+    const currentElem = e.currentTarget.parentElement.parentElement;
+    currentElem.style.display = "none";
   }
 
   // END CLASS
