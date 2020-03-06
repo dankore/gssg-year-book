895aa5cb7247c3160a9c8680c15d423174a287ce
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-03-06 08:36:13 -0600

    likes feature: like btn changes color when clicked

--- frontend-js/modules/likes.js
+++ frontend-js/modules/likes.js
@@ -2,7 +2,9 @@ const axios = require("axios");
 export default class Likes {
   constructor() {
     this.likesButton = document.querySelector("#likes-button");
-    this.likesContainer = document.querySelector("#likes-container");
+    this.likesContainer = document.querySelector("#likes-container"); 
+    this.likesButtonSVG = document.querySelector("#like-button-svg");
+    this.likeWordContainer = document.querySelector("#like-word");
     this.events();
   }
 
@@ -17,10 +19,14 @@ export default class Likes {
       if(this.likesButton.classList.contains("like")){
           this.likesButton.classList.remove("like");
           this.likesButton.classList.add("dislike");
+          this.likesButtonSVG.style.fill = "#3182ce";
+          this.likeWordContainer.style.color = "#3182ce";
           like = 1;
       } else {
           this.likesButton.classList.add("like");
           this.likesButton.classList.remove("dislike");
+          this.likesButtonSVG.style.fill = "white";
+          this.likeWordContainer.style.color = "black";
           like = -1;
       }
       console.log("like value: " + like);
