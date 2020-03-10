e3dd14484d31bc8993a077cf49d76cb04cb14b87
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-03-10 14:45:28 -0500

    add el

--- frontend-js/modules/show-those-liked-a-profile.js
+++ frontend-js/modules/show-those-liked-a-profile.js
@@ -4,31 +4,32 @@ export default class ShowNames {
       "#like-button-svg-container"
     );
     this.namesContainer = document.querySelector("#names-container");
-    this.main = document.querySelector("main");
     this.events();
   }
   // EVENTS
   events() {
-    this.likeButtonSVGContainer.addEventListener("mouseover", () =>
-      this.handleMouseOver()
-    );
     this.likeButtonSVGContainer.addEventListener("click", () =>
-      this.handleMouseOver()
+      this.handleButtonClick()
     );
-    this.likeButtonSVGContainer.addEventListener("click", () =>
-      this.handleMouseOut()
-    );
+    this.likeButtonSVGContainer.addEventListener("mouseover", ()=> this.handleMouseOver());
     this.likeButtonSVGContainer.addEventListener("mouseout", () =>
       this.handleMouseOut()
     );
   }
 
   // METHODS
-  handleMouseOver() {
+  handleMouseOver(){
     this.namesContainer.style.display = "block";
   }
-  handleMouseOut() {
+  handleMouseOut(){
     this.namesContainer.style.display = "none";
+  }
+  handleButtonClick(){
+    if(this.namesContainer.style.display == "none"){
+      this.namesContainer.style.display = "block";
+    } else {
+      this.namesContainer.style.display = "none";
+    }
   }
  
   // END CLASS
