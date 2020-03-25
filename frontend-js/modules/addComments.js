81d6322aa3c670341fe9c7c7cc325e9b6a966d89
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-03-25 04:30:22 -0500

    add el to edit btn

--- frontend-js/modules/addComments.js
+++ frontend-js/modules/addComments.js
@@ -15,15 +15,24 @@ export default class AddComments {
   }
   // EVENTS
   events() {
-    this.addCommentButton.addEventListener("click", () => this.handleAddCommentClick());
+    this.addCommentButton.addEventListener("click", () =>
+      this.handleAddCommentClick()
+    );
     this.document.addEventListener("click", e => {
       if (e.target && e.target.id == "delete-comment-button") {
         this.handleDeleteComment(e);
       }
+       if (e.target && e.target.id == "edit-comment-button") {
+         this.handleEditComment(e);
+       }
     });
   }
 
   // METHODS
+  handleEditComment(e){
+    console.log(e.target)
+  }
+
   handleDeleteComment(e) {
     if (confirm("Are you sure?")) {
       axios
@@ -108,13 +117,9 @@ export default class AddComments {
         <p>
           ${data.commentDate}
         </p>
-         <!-- EDIT BUTTON -->
+        <!--__EDIT BUTTON_____________-->
         <div class="flex">
-          <button
-            id="edit-button"
-            value="${data.comment}"
-            class="flex items-center"
-          >
+          <label for="edit-comment-button" class="flex items-center cursor-pointer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -127,9 +132,14 @@ export default class AddComments {
                 d="M6.3 12.3l10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H7a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM8 16h2.59l9-9L17 4.41l-9 9V16zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h6a1 1 0 0 1 0 2H4v14h14v-6z"
               />
             </svg>
-            <p>Edit</p>
-          </button>
-          <!-- EDIT BUTTON ENDS-->
+            <input
+              type="button"
+              value="Edit"
+              id="edit-comment-button"
+              class="flex bg-white items-center cursor-pointer"
+            />
+          </label>
+         <!--__EDIT BUTTON ENDS_____________-->
 
           <!-- DELETE BUTTON -->
           <label class="flex items-center ml-2 cursor-pointer" for="delete-comment-button">
@@ -150,7 +160,7 @@ export default class AddComments {
               value="Delete"
               id="delete-comment-button"
               data-id="${data.commentId}"
-              class="delete-comment flex bg-white items-center cursor-pointer"
+              class="flex bg-white items-center cursor-pointer"
             />
             </label>
             <!-- DELETE BUTTON ENDS -->
