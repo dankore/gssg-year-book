b57029b2577541773b7f66c3aec3704c21f61d4a
Adamu Muhammad Dankore <adamu.dankore@gmail.com>
2020-03-29 01:50:50 +0000

    Some minor changes

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -5,49 +5,25 @@ import ToggleImage from "./modules/show_bigger_profile_image";
 import ConfirmDelete from "./modules/confirmDelete";
 import RegistrationFormLiveValidation from "./modules/registrationFormLiveValidation";
 import Filter from "./modules/filter";
-import EditCancelUpdateComments from "./modules/comments-edit-cancel-update-btn";
 import ShowAllComments from "./modules/comments-show-all";
 import Likes from "./modules/likes";
 import ShowNames from "./modules/show-those-liked-a-profile";
 import ClickToComment from "./modules/clickToComment";
 import AddComment from "./modules/addComments";
 
-if (document.querySelector("#more-profile-btn")) {
-  new ToggleProfile();
-}
-if (document.querySelector("#btn-optional-fields")) {
-  new ToggleEditPage();
-}
-if (document.querySelector("#bar-chart-icon-container")) {
-  new ToggleStats();
-}
-if (document.querySelector("#profile-image")) {
-  new ToggleImage();
-}
-if (document.querySelector("#anchor-tag")) {
-  new ToggleImage();
-}
-if (document.querySelector("#delete-profile")) {
-  new ConfirmDelete();
-}
-if (document.querySelector("#registration-form")) {
+document.querySelector("#more-profile-btn") && new ToggleProfile();
+document.querySelector("#btn-optional-fields") && new ToggleEditPage();
+document.querySelector("#bar-chart-icon-container") && new ToggleStats();
+document.querySelector("#profile-image") && new ToggleImage();
+document.querySelector("#anchor-tag") && new ToggleImage();
+document.querySelector("#delete-profile") && new ConfirmDelete();
+document.querySelector("#registration-form") &&
   new RegistrationFormLiveValidation();
-}
-if (document.querySelector("#filter-icon-container")) {
-  new Filter();
-}
-if (document.querySelector("#show-all-comments-toggle")) {
-  new ShowAllComments();
-}
-if (document.querySelector("#likes-button")) {
-  new Likes();
-}
-if (document.querySelector("#likes-container")) {
-  new ShowNames();
-}
-if (document.getElementById("click-to-comment")) {
-  new ClickToComment();
-}
-if (document.getElementById("button-comment")) {
-  new AddComment();
-}
+document.querySelector("#registration-form") &&
+  new RegistrationFormLiveValidation();
+document.querySelector("#filter-icon-container") && new Filter();
+document.querySelector("#show-all-comments-toggle") && new ShowAllComments();
+document.querySelector("#likes-button") && new Likes();
+document.querySelector("#likes-container") && new ShowNames();
+document.getElementById("click-to-comment") && new ClickToComment();
+document.getElementById("button-comment") && new AddComment();
